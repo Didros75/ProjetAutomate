269c7561f64207501cbdf6f5cc375728f876b590
Julie-sx <julie.sieux@efrei.net>
2026-03-25 16:28:15 +0100

    reconnaissance des mots

--- julie.py
+++ julie.py
@@ -73,23 +73,58 @@ def afficher_transitions(auto):
             str_ligne += "\t|\t"
         print(str_ligne)
 
-afficher_automate({'nb_etats': 5, 'alphabet': ['a', 'b'], 'initiaux': [1, 3], 'finaux': [2, 4], 'transitions': [(1, 'a', 2), (1, 'b', 0), (3, 'a', 0), (3, 'b', 4), (0, 'a', 0), (0, 'b', 0)]})
+#afficher_automate({'nb_etats': 5, 'alphabet': ['a', 'b'], 'initiaux': [1, 3], 'finaux': [2, 4], 'transitions': [(1, 'a', 2), (1, 'b', 0), (3, 'a', 0), (3, 'b', 4), (0, 'a', 0), (0, 'b', 0)]})
 
 # Reconnaissance de mots
 
 def lire_mot(mot) :
     """
+    Demande un mot à vérifier à l'utilisateur
+    :param mot: le mot, une chaine de caractère
+    :return: le mot
+    """
+    print("Ecrire un mot à vérifier (écrire fin pour stopper la vérification) :")
+    mot = ""
+    return input(mot)
 
-    :param mot:
-    :return:
+def reconnaitre_mot(mot, auto) :
+    """
+    Vérifie si le mot donné en paramètre est reconnu par l'automate ou non
+    :param mot: le mot à vérifier, chaine de caractère
+    :param A: un dictionnaire rprésentant l'automate
+    :return: oui si le mot est reconnu, non sinon
     """
 
-def reconnaitre_mot() :
-    pass
+    # ATTENTION : Utiliser la fonction de Melvin pour déterminiser
 
-def reconnaissance() :
+    etat_actuel = auto["initiaux"][0]
+
+    while mot != "":
+        transition_trouvee = False
+
+        for elem in auto['transitions']:
+            if etat_actuel == elem[0] and mot[0] == elem[1]:
+                etat_actuel = elem[2]
+                mot = mot[1:]
+                transition_trouvee = True
+                break
+
+        if not transition_trouvee:
+            return "non"
+
+    if etat_actuel in auto["finaux"]:
+        return "oui"
+    return "non"
+
+def reconnaissance(auto) :
     """
-
+    lance la reconnaissance des mots jusq'au mot "fin"
     :return: none
     """
-    pass
+    mot = ""
+    while mot != "fin" :
+        mot = lire_mot(mot)
+        reponse = reconnaitre_mot(mot, auto)
+        print(reponse)
+
+reconnaissance({'nb_etats': 5, 'alphabet': ['a', 'b'], 'initiaux': [1, 3], 'finaux': [2, 4], 'transitions': [(1, 'a', 2), (1, 'b', 0), (3, 'a', 0), (3, 'b', 4), (0, 'a', 0), (0, 'b', 0)]})
